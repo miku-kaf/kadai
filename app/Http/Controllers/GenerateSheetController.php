3ec9d0cf75dea1268b90a3eeafb3a315532d7173
miku-kaf <66938821+miku-kaf@users.noreply.github.com>
2022-02-16 15:38:23 +0900

    Achievement 4

--- app/Http/Controllers/GenerateSheetController.php
+++ app/Http/Controllers/GenerateSheetController.php
@@ -11,7 +11,8 @@ class GenerateSheetController extends Controller
         $num=$request->input("number");
         $l=pow($num,2);
 
-        $link = mysqli_connect('mysql57.tcagame01.sakura.ne.jp', 'tcagame01', 'tcagame2021', 'tcagame01_20j70041');
+        $link = mysqli_connect('localhost', 'root', '', 'network');
+        //$link = mysqli_connect('mysql57.tcagame01.sakura.ne.jp', 'tcagame01', 'tcagame2021', 'tcagame01_20j70041');
         $stmt = mysqli_prepare($link, "INSERT INTO results (result) VALUES (?)");
         mysqli_stmt_bind_param($stmt, "s", $l);
         $result = mysqli_stmt_execute($stmt);
--- app/Http/Controllers/area_circleController.php
+++ app/Http/Controllers/area_circleController.php
@@ -11,7 +11,8 @@ class area_circleController extends Controller
         $num=$request->input("number");
         $datanum =pow($num,2)*3.14;
 
-        $link = mysqli_connect('mysql57.tcagame01.sakura.ne.jp', 'tcagame01', 'tcagame2021', 'tcagame01_20j70041');
+        $link = mysqli_connect('localhost', 'root', '', 'network');
+        //$link = mysqli_connect('mysql57.tcagame01.sakura.ne.jp', 'tcagame01', 'tcagame2021', 'tcagame01_20j70041');
         $stmt = mysqli_prepare($link, "INSERT INTO results (result) VALUES (?)");
         mysqli_stmt_bind_param($stmt, "d", $datanum);
         $result = mysqli_stmt_execute($stmt);
